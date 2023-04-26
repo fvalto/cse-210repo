8a4b7835b60c4e8818e0cfab9b368c6410fe09af
fvalto <skualo_fv@msn.com>
2023-04-25 23:37:22 -0400

    Prep 3, 4 and 5 Done!

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int target;
+        int guess;
+        int tries = 0;
+        // Console.Write("What is the magic number?: ");
+        // target = int.Parse(Console.ReadLine());
+        Random randomGenerator = new Random();
+        target = randomGenerator.Next(0, 101);
+
+        do 
+        {
+            Console.Write("What is your guess?: ");
+            tries++;
+            guess = int.Parse(Console.ReadLine());
+            if (target > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (target < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+                Console.WriteLine($"It took you {tries} tries.");
+            }
+        } while (target != guess);
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,32 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int toAdd;
+        int total;
+        float average;
+        int largest;
+
+        do
+        {
+            Console.Write("Enter number: ");
+            toAdd = int.Parse(Console.ReadLine());
+            if (toAdd != 0)
+            {
+                numbers.Add(toAdd);
+            }
+        } while (toAdd != 0);
+        
+        total = numbers.Sum();
+        average = (float)total/(numbers.Count);
+        largest = numbers.Max();
+
+        Console.WriteLine($"The sum is: {total}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largest}");
     }
 }
