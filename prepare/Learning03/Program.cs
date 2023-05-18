483678c21d713d880fe37cd31e5c27a6515d0ce9
fvalto <skualo_fv@msn.com>
2023-05-18 00:12:00 -0400

    Learning03 Done!

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction firstExample = new Fraction();
+        Console.WriteLine(firstExample.GetFractionString());
+        Console.WriteLine(firstExample.GetDecimalValue());
+
+        Fraction secondExample = new Fraction(5);
+        Console.WriteLine(secondExample.GetFractionString());
+        Console.WriteLine(secondExample.GetDecimalValue());
+
+        Fraction thirdExample = new Fraction(3, 4);
+        Console.WriteLine(thirdExample.GetFractionString());
+        Console.WriteLine(thirdExample.GetDecimalValue());
+
+        Fraction fourthExample = new Fraction(1, 3);
+        Console.WriteLine(fourthExample.GetFractionString());
+        Console.WriteLine(fourthExample.GetDecimalValue());
     }
 }
